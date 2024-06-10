97fab5ce89de34b63e5436136dcb1c4222747a2f
dm12354 <ritu.agarwal0883@gmail.com>
2024-06-10 17:12:09 +0530

    Update whats_new.py

--- whats_new.py
+++ whats_new.py
@@ -4,18 +4,18 @@ def app():
     st.title("What's New ?")
     st.markdown(
         """
-        ## Version 1.0.4 beta (Updated on June 10,2024)
+        ## Version 1.0.4 (Updated on June 10,2024)
         - Added background in sidebar menu.
         - Auto-closing sidebar menu.
         
-        ## Version 1.0.3 beta (Updated on June 7,2024)
+        ## Version 1.0.3 (Updated on June 7,2024)
         - Added icons in sidebar menu.
         - Fixed bugs.
         
         ## Version 1.0.2 (Updated on June 3,2024)
         - Added Stable Diffusion image generation.
         - Initial release with core functionalities.
-        - Integrated with Gemini AI for chat responses.
+        - Integrated with Gemini 1.5 instead of 1.0 for chat responses.
         - Enhanced UI for better user experience.
         
         ## Version 1.0.1 (Updated on May 31,2024)

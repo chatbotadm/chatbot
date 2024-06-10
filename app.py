3b849c41080959dabb87820d3762a8ac1f43d896
dm12354 <ritu.agarwal0883@gmail.com>
2024-06-10 22:31:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,4 @@ def index():
 if __name__ == "__main__":
     socketio.run(app)
     python3 __init__.py
+    st.markdown("<p style='text-align: center; font-size: 10px;'>Note: This is a beta version and may contain bugs.</p>", unsafe_allow_html=True)
